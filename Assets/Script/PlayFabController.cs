a65fac7ff1d61ca261e056bca8622f0541c745a8
R <rockleeserver3@gmail.com>
2021-05-25 14:28:48 +0900

    ログイン機能　名前付きで実装

--- Assets/Script/PlayFabController.cs
+++ Assets/Script/PlayFabController.cs
@@ -1,14 +1,40 @@
 ﻿using PlayFab;
 using PlayFab.ClientModels;
 using UnityEngine;
+using System.Collections;
 
 public class PlayFabController : MonoBehaviour {
-    void Start() {
-        //PlayFabClientAPI.LoginWithCustomID(
-        //    new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true }
-        //, result => Debug.Log("おめでとうございます！ログイン成功です！")
-        //, error => Debug.Log("ログイン失敗...(´；ω；｀)"));
 
+    public string PlayFabId;
+
+    void OnEnable() {
+        PlayFabAuthService.OnLoginSuccess += PlayFabLogin_OnLoginSuccess;
+    }
+
+    IEnumerator Start() {
         PlayFabAuthService.Instance.Authenticate(Authtypes.Silent);
+        yield return new WaitForSeconds(3.0f);
+        GetUserData();
+    }
+
+    private void PlayFabLogin_OnLoginSuccess(LoginResult result) {
+        Debug.Log("Login Success!");
+
+    }
+    private void OnDisable() {
+        PlayFabAuthService.OnLoginSuccess -= PlayFabLogin_OnLoginSuccess;
+    }
+
+    void GetUserData() {
+        PlayFabClientAPI.GetUserData(new GetUserDataRequest() {
+            PlayFabId = PlayFabId
+        }, result => {
+            Debug.Log(result.Data["Name"].Value);
+            Debug.Log("ログイン成功");
+        }, error => {
+            Debug.Log(error.GenerateErrorReport());
+            Debug.Log("ログイン失敗");
+
+        });
     }
 }
